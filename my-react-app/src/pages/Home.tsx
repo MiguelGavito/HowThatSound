ca91f36d3be9f37d1fff9af666e0c3a5ac85090b
MiguelGavito <mgavitogzz@gmail.com>
2025-05-13 17:47:30 -0600

    guarde este frontend basico que funciona de basico, luego voy a descargar tailwind para empezar a practicar bien, esta version solo tiene el my-react-app para el proyecto y practicar el dise;o que quiero

---
+++ my-react-app/src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar />
+      <h1>Bienvenido a mi pagina de inicio</h1>
+    </div>
+  );
+};
+
+export default Home;
